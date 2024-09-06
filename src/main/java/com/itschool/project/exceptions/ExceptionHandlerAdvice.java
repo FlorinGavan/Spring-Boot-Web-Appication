51b5de42d3af7b09894a12895c161dedf1c348a5
Florin <f_ionut47@yahoo.com>
2024-09-06 17:21:50 +0300

    Exceptions

--- src/main/java/com/itschool/project/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/com/itschool/project/exceptions/ExceptionHandlerAdvice.java
@@ -3,7 +3,6 @@ package com.itschool.project.exceptions;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itschool.project.exceptions.practice.UserCreateException;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
